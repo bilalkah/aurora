35b18c7d1877278060947f41d951aa03f7da047c
Bilal Kahraman <kahramannbilal@gmail.com>
2021-09-05 02:22:19 +0300

    error fix

--- monitor.py
+++ monitor.py
@@ -13,7 +13,6 @@ class ThreadedVideoStream:
         self.stream.set(cv2.CAP_PROP_FRAME_WIDTH, 1920)
         self.stream.set(cv2.CAP_PROP_FRAME_HEIGHT, 1080)
         (self.grabbed, self.frame) = self.stream.read()
-        self.q_out = q_out
         # color detection
         self.center = (int(self.stream.get(cv2.CAP_PROP_FRAME_WIDTH)/2), int(self.stream.get(cv2.CAP_PROP_FRAME_HEIGHT)/2))
         self.colorDict = {
