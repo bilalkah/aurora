858c8c83baa74cc0b95a5d9ab77a27bdac1417a1
Bilal Kahraman <kahramannbilal@gmail.com>
2021-09-06 03:25:25 +0300

    red color

--- test.py
+++ test.py
@@ -6,21 +6,11 @@ from math import ceil
 
 if __name__ == "__main__":
     myThread = ThreadedVideoStream(livestream=True, stream_address=('192.168.137.61',9999))
-    myThread.setColor("blue")
+    myThread.setColor("red")
 
-    start = time.time()
-    i = 0
-    j = 0
     try:
         while True:
-            print(myThread.getColor())
-            for i in range(10):
-                time.sleep(1)
-            
-            if myThread.getColor() == "blue":
-                myThread.setColor("red")
-            else:
-                myThread.setColor("blue")
+            continue
                 
     except KeyboardInterrupt:
         myThread.finish()
