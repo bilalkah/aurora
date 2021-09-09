9f0fc04697e8f2509ab1b840c3d0aa5ddb116914
Bilal Kahraman <kahramannbilal@gmail.com>
2021-09-09 14:01:33 +0300

    test

--- livestream.py
+++ livestream.py
@@ -12,25 +12,34 @@ def dump_buffer(s):
         if struct.unpack("B",seg[0:1])[0] == 1:
             print("finish emptying buffer")
             break
-s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
 
 def main():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     s.bind(('192.168.43.233',12345))
     dat = b''
     dump_buffer(s)
     while True:
-        seg, addr = s.recvfrom(MAX_DGRAM)
-        if struct.unpack("B",seg[0:1])[0] > 1:
-            dat += seg[1:]
-        else:
-            dat += seg[1:]
-            img = cv2.imdecode(np.frombuffer(dat, dtype=np.uint8), 1)
-            cv2.imshow("from drone", img)
-            if cv2.waitKey(1) & 0xFF == ord('q'):
-                break
+        try:
+            seg, addr = s.recvfrom(MAX_DGRAM)
+            if struct.unpack("B",seg[0:1])[0] > 1:
+                dat += seg[1:]
+            else:
+                dat += seg[1:]
+                img = cv2.imdecode(np.frombuffer(dat, dtype=np.uint8), 1)
+                cv2.imshow("from drone", img)
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+                dat = b''
+        except KeyboardInterrupt:
+            break
+        except BaseException as e:    
+            print("hata, "+str(e))
+            s.close()
+            s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+            s.bind(('192.168.43.233',12345))
             dat = b''
-        
-
+            dump_buffer(s)
 try:
     main()
 except KeyboardInterrupt:
